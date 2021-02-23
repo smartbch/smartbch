6985d077f2597deccc36bcae97561737b431cd79
matrix <lhrmachinery@163.com>
2021-02-23 16:01:22 +0800

    Modify build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-cd evmwrap; make
-cd ..
+pushd ../MoeingEVM/evmwrap || return; make
+popd || return
 
-cp evmwrap/host_bridge/libevmwrap.so .
-cp evmwrap/host_bridge/libevmwrap.so app
-cp evmwrap/host_bridge/libevmwrap.so cmd/moeingd
-cp evmwrap/host_bridge/libevmwrap.so cmd/moeingcli
-cp evmwrap/host_bridge/libevmwrap.so ebp
-cp evmwrap/host_bridge/libevmwrap.so ebp/ebptests
-cp evmwrap/host_bridge/libevmwrap.so rpc/api
-cp evmwrap/host_bridge/libevmwrap.so rpc/api/filters
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/ebp
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/ebp/ebptests
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/evmwrap/evmwrap/evmtests
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/evmwrap/evmwrap/host_bridge
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so .
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so app
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so cmd/moeingd
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so cmd/moeingcli
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/api
+cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/api/filters
+
 go build ./...
 go test ./...
