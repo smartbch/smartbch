59a9e26f284ea161de5fd0cc0a6c7071310bc992
zxh0 <zhangxiuhong2013@gmail.com>
2021-03-02 11:20:44 +0800

    Add some unit tests for FilterCriteria unmarshal logic

--- internal/testutils/hex.go
+++ internal/testutils/hex.go
@@ -3,7 +3,25 @@ package testutils
 import (
 	"encoding/hex"
 	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
 )
+
+func HexToHash32(s string) common.Hash {
+	if strings.HasPrefix(s, "0x") {
+		s = s[2:]
+	}
+
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+
+	hash := common.Hash{}
+	copy(hash[:], bytes)
+
+	return hash
+}
 
 func HexToBytes(s string) []byte {
 	if strings.HasPrefix(s, "0x") {
---
+++ rpc/api/filters/geth_filter_test.go
@@ -0,0 +1,52 @@
+package filters
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/eth/filters"
+
+	"github.com/moeing-chain/moeing-chain/internal/testutils"
+)
+
+func TestFilterCriteria1(t *testing.T) {
+	data := `
+{
+  "fromBlock": "0x1",
+  "toBlock": "0x2",
+  "address": "0x8888f1f195afa192cfee860698584c030f4c9db1",
+  "topics": [
+    ["0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b", null],
+    [null, "0x0000000000000000000000000aff3454fce5edbc8cca8697c15331677e6ebccc"]
+  ]
+}
+`
+	fc := filters.FilterCriteria{}
+	err := json.Unmarshal([]byte(data), &fc)
+	require.NoError(t, err)
+	require.Equal(t, [][]common.Hash{[]common.Hash(nil), []common.Hash(nil)}, fc.Topics)
+}
+
+func TestFilterCriteria2(t *testing.T) {
+	data := `
+{
+  "fromBlock": "0x1",
+  "toBlock": "0x2",
+  "address": "0x8888f1f195afa192cfee860698584c030f4c9db1",
+  "topics": [
+    null,
+    "0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b"
+  ]
+}
+`
+	fc := filters.FilterCriteria{}
+	err := json.Unmarshal([]byte(data), &fc)
+	require.NoError(t, err)
+	require.Equal(t, [][]common.Hash{
+		[]common.Hash(nil),
+		{testutils.HexToHash32("0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b")},
+	}, fc.Topics)
+}
