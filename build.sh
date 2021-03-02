67e1e29b22b03f57e2cf9f806b676b5e94831d2b
zxh0 <zhangxiuhong2013@gmail.com>
2021-03-02 12:55:16 +0800

    Upgrade moeing tools

--- build.sh
+++ build.sh
@@ -3,18 +3,8 @@
 pushd ../MoeingEVM/evmwrap || return; make clean; make
 popd || return
 
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/ebp
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/ebp/ebptests
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/evmwrap/evmwrap/evmtests
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so ../MoeingEVM/evmwrap/evmwrap/host_bridge
 cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so .
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so app
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so cmd/moeingd
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so cmd/moeingcli
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/api
-cp ../MoeingEVM/evmwrap/host_bridge/libevmwrap.so rpc/api/filters
+export EVMWRAP=$PWD/libevmwrap.so
 
 go build ./...
 go test ./...
--- go.mod
+++ go.mod
@@ -5,13 +5,12 @@ go 1.15
 require (
 	github.com/ethereum/go-ethereum v1.9.25
 	github.com/holiman/uint256 v1.1.1
-	github.com/moeing-chain/MoeingADS v0.0.0-20210208154249-d4465b91aa22
-	github.com/moeing-chain/MoeingDB v0.0.0-20210205013121-7d93963898a1
-	github.com/moeing-chain/MoeingEVM v0.0.0-20210301080654-258dc8856e20
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/moeing-chain/MoeingADS v0.0.0-20210301000353-f63a7437981f
+	github.com/moeing-chain/MoeingDB v0.0.0-20210301213242-555118913112
+	github.com/moeing-chain/MoeingEVM v0.0.0-20210302033851-893802a82edd
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tendermint/tendermint v0.34.7
 )
-
-replace github.com/moeing-chain/MoeingEVM v0.0.0-20210301080654-258dc8856e20 => ../MoeingEVM
