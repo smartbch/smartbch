98f548373f401dc56058fa1f5aee4bacf557a092
zxh0 <zhangxiuhong2013@gmail.com>
2021-03-17 14:07:33 +0800

    Rename package: cmd/moeingd -> cmd/smartbchd

--- restart_from_h0.sh
+++ restart_from_h0.sh
@@ -7,4 +7,4 @@ go run github.com/smartbch/smartbch/cmd/smartbchd init m1 --chain-id 0x1
 export NODIASM=1 
 export NOSTACK=1
 export NOINSTLOG=1
-go run github.com/smartbch/smartbch/cmd/moeingd start
+go run github.com/smartbch/smartbch/cmd/smartbchd start
