9e63a6cf0522595d62319408d871a2161fa1fe73
zxh0 <zhangxiuhong2013@gmail.com>
2021-04-12 15:12:54 +0800

    Create sep206.js

---
+++ testdata/seps/test/sep206.js
@@ -0,0 +1,12 @@
+const IERC20 = artifacts.require("IERC20");
+
+contract("SEP206", async (accounts) => {
+
+    it('basic info', async () => {
+        const sep206 = new IERC20("0x0000000000000000000000000000000000002711");
+        assert.equal(await sep206.name(), "BCH");
+        assert.equal(await sep206.symbol(), "BCH");
+        assert.equal(await sep206.decimals(), 18);
+    });
+
+});
