1cc4ff44734663a64410b290cd18f4718ffb978b
zxh0 <zhangxiuhong2013@gmail.com>
2021-04-22 15:26:54 +0800

    Improve BLOCKHASH truffle test

--- testdata/basic/test/block_hash.js
+++ testdata/basic/test/block_hash.js
@@ -5,16 +5,22 @@ contract("BlockHash", async (accounts) => {
     it('getBlockHash', async () => {
         const contract = await BlockHash.new();
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 5; i++) {
+            await web3.eth.sendTransaction({from: accounts[0], to: accounts[1], value: 10000});
+        }
+
+        for (let i = 0; i < 5; i++) {
             await web3.eth.sendTransaction({from: accounts[0], to: accounts[1], value: 10000});
 
             let blockNum = await web3.eth.getBlockNumber();
-            let block = await web3.eth.getBlock(blockNum);
-            let blockHash = await contract.getBlockHash(blockNum);
-            console.log("block number: ", blockNum);
-            console.log(block.hash);
-            console.log(blockHash);
-            assert.equal(blockHash, block.hash);
+            for (let j = 0; j < 5; j++, blockNum--) {
+                let block = await web3.eth.getBlock(blockNum);
+                let blockHash = await contract.getBlockHash(blockNum);
+                console.log("block number: ", blockNum);
+                console.log(block.hash);
+                console.log(blockHash);
+                assert.equal(blockHash, block.hash);
+            }
         }
     });
 
