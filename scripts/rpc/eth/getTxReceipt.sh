477188c160845254d080a28476e5d906581c89da
zxh0 <zhangxiuhong2013@gmail.com>
2021-05-17 18:17:33 +0800

    Add some scripts

---
+++ scripts/rpc/eth/getTxReceipt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+REQ=$(cat <<-END
+{
+  "jsonrpc":"2.0",
+  "method":"eth_getTransactionReceipt",
+  "params":["$1"],
+  "id":1
+}
+END
+)
+
+# echo ">>>" $REQ
+# printf "<<< "
+curl -X POST \
+	--data "$REQ" \
+	-H "Content-Type: application/json" \
+	"${RPC_URL:-http://moeing.app:8545}"
