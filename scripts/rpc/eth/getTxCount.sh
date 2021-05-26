fccd696bc04f6ffe4218850d5d96dc879b53be99
zxh0 <zhangxiuhong2013@gmail.com>
2021-05-26 17:24:35 +0800

    Create getTxCount.sh

---
+++ scripts/rpc/eth/getTxCount.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+REQ=$(cat <<-END
+{
+  "jsonrpc":"2.0",
+  "method":"eth_getTransactionCount",
+  "params":["$1", "latest"],
+  "id":1
+}
+END
+)
+
+# echo ">>>" $REQ
+# printf "<<< "
+curl -X POST \
+	--data "$REQ" \
+	-H "Content-Type: application/json" \
+	"${RPC_URL:-http://moeing.app:8545}"
