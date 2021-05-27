762ad176b5bd5ab48398f5be2ddad57e10b223fb
zxh0 <zhangxiuhong2013@gmail.com>
2021-05-27 14:52:26 +0800

    Code refactor

--- cmd/stresstest/replay_ws.go
+++ cmd/stresstest/replay_ws.go
@@ -25,18 +25,7 @@ func RunReplayBlocksWS(url string) {
 	defer c.Close()
 
 	blkDB := NewBlockDB(blockDir)
-
-	allBlocks := uint32(1)
-	for blkDB.LoadBlock(allBlocks) != nil {
-		fmt.Printf("\rtotal blocks: %d", allBlocks)
-		allBlocks += 100
-	}
-	allBlocks -= 100
-	for blkDB.LoadBlock(allBlocks) != nil {
-		fmt.Printf("\rtotal blocks: %d", allBlocks)
-		allBlocks++
-	}
-	fmt.Println()
+	allBlocks := getTotalHeight(blkDB)
 
 	h := uint32(0)
 	retryCount := 10
@@ -61,37 +50,60 @@ func RunReplayBlocksWS(url string) {
 			}
 			fmt.Printf("\rblock: %d, tx: %d; total sent tx: %d, total failed tx: %d, time: %ds, tps:%d, progress:%f%%",
 				h, i, okTxCount, failedTxCount, timeElapsed, tps, float64(h)/float64(allBlocks)*100)
-			for i := 0; i < retryCount; i++ {
-				//time.Sleep(100 * time.Millisecond)
-				resp := sendRawTxWS(tx, c, false)
-				if !bytes.Contains(resp, []byte("error")) {
-					okTxCount++
-					break // ok
-				}
-
-				// retry
-				if i < retryCount-1 {
-					time.Sleep(50 * time.Millisecond)
-				} else {
-					fmt.Println("failed to send tx:", string(resp))
-					failedTxCount++
-				}
+			if sendRawTxWithRetry(c, tx, false, retryCount) {
+				okTxCount++
+			} else {
+				failedTxCount++
 			}
 		}
 	}
 	fmt.Println("\nDONE!")
 }
 
-func sendRawTxWS(tx []byte, c *websocket.Conn, printsLog bool) []byte {
+func getTotalHeight(blkDB *BlockDB) uint32 {
+	h := uint32(1)
+	for blkDB.LoadBlock(h) != nil {
+		fmt.Printf("\rtotal blocks: %d", h)
+		h += 100
+	}
+	h -= 100
+	for blkDB.LoadBlock(h) != nil {
+		fmt.Printf("\rtotal blocks: %d", h)
+		h++
+	}
+	fmt.Println()
+	return h
+}
+
+func sendRawTxWithRetry(c *websocket.Conn, tx []byte, logsMsg bool, retryCount int) bool {
+	for i := 0; i < retryCount; i++ {
+		//time.Sleep(100 * time.Millisecond)
+		resp := sendRawTx(c, tx, logsMsg)
+		if !bytes.Contains(resp, []byte("error")) {
+			return true
+		}
+
+		// retry
+		if i < retryCount-1 {
+			time.Sleep(50 * time.Millisecond)
+		} else {
+			fmt.Println("failed to send tx:", string(resp))
+			return false
+		}
+	}
+	return false
+}
+
+func sendRawTx(c *websocket.Conn, tx []byte, logsMsg bool) []byte {
 	reqID++
 	sendRawTxReq := fmt.Sprintf(sendRawTxReqFmt, "0x"+hex.EncodeToString(tx), reqID)
-	if printsLog {
+	if logsMsg {
 		fmt.Println("write:", sendRawTxReq)
 	}
 
 	err := c.WriteMessage(websocket.TextMessage, []byte(sendRawTxReq))
 	if err != nil {
-		if printsLog {
+		if logsMsg {
 			fmt.Println("write error:", err)
 		}
 		return []byte("error:" + err.Error())
@@ -99,12 +111,12 @@ func sendRawTxWS(tx []byte, c *websocket.Conn, printsLog bool) []byte {
 
 	_, resp, err := c.ReadMessage()
 	if err != nil {
-		if printsLog {
+		if logsMsg {
 			fmt.Println("read error:", err)
 		}
 		return []byte("error:" + err.Error())
 	}
-	if printsLog {
+	if logsMsg {
 		fmt.Println("read:", string(resp))
 	}
 	return resp
