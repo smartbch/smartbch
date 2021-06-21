2796fc91ac0b4cd5891a9e6c1d90d03cf103d8b1
zxh0 <zhangxiuhong2013@gmail.com>
2021-06-21 15:38:25 +0800

    Optimize staking RPC client

---
+++ staking/rpcclient_parallel.go
@@ -0,0 +1,66 @@
+package staking
+
+import (
+	"sync"
+
+	"github.com/smartbch/smartbch/staking/types"
+)
+
+var _ types.RpcClient = (*ParallelRpcClient)(nil)
+
+type ParallelRpcClient struct {
+	client       *RpcClient
+	rwLock       sync.RWMutex
+	latestHeight int64
+	preGetMaxH   int64
+	preGetBlocks map[int64]*types.BCHBlock
+}
+
+func NewParallelRpcClient(url, user, password string) types.RpcClient {
+	return &ParallelRpcClient{client: NewRpcClient(url, user, password)}
+}
+
+func (c *ParallelRpcClient) GetBlockByHash(hash [32]byte) *types.BCHBlock {
+	return c.client.GetBlockByHash(hash)
+}
+
+func (c *ParallelRpcClient) GetLatestHeight() int64 {
+	c.latestHeight = c.client.GetLatestHeight()
+	return c.latestHeight
+}
+
+func (c *ParallelRpcClient) GetBlockByHeight(height int64) *types.BCHBlock {
+	if height+20 < c.latestHeight {
+		c.rwLock.RLock()
+		block, found := c.preGetBlocks[height]
+		c.rwLock.Unlock()
+
+		if found {
+			return block
+		}
+
+		if c.preGetMaxH < height {
+			c.preGetMaxH = height + 11
+			go c.getBlocksAsync(height+1, 10)
+		}
+	}
+
+	return c.client.GetBlockByHeight(height)
+}
+
+func (c *ParallelRpcClient) getBlocksAsync(hStart, n int64) {
+	c.rwLock.Lock()
+	c.preGetBlocks = map[int64]*types.BCHBlock{}
+	c.rwLock.Unlock()
+
+	for i := int64(0); i < n; i++ {
+		h := hStart + i
+		go func() {
+			if b := c.client.GetBlockByHeight(h); b != nil {
+				c.rwLock.Lock()
+				c.preGetBlocks[h] = b
+				c.rwLock.Unlock()
+			}
+		}()
+	}
+}
