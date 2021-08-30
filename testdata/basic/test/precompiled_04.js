4972fa635a3cd48431562737dce2c1ca1a13cfd6
zxh0 <zhangxiuhong2013@gmail.com>
2021-08-30 17:01:38 +0800

    Update precompiled_04.js

--- testdata/basic/test/precompiled_04.js
+++ testdata/basic/test/precompiled_04.js
@@ -8,8 +8,8 @@ contract("Precompiled", async (accounts) => {
         const ret1 = await p04.callDatacopy1.call('0x11223344556677889900');
         assert.equal(ret1, '0x11223344556677889900');
 
-        const ret2 = await p04.callDatacopy2.call('0x11223344556677889900');
-        assert.equal(ret2, '0x11223344556677112233');
+        const ret2 = await p04.callDatacopy2.call('0x111111112222222233333333444444445555555566666666777777778888888899999999aaaaaaaa');
+        assert.equal(ret2, '0x11111111222222111111112222222233333333444444445555555566666666777777778888888899');
     });
 
 });
