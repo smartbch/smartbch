ad07e025b17523ce97c1a75215b35f0b9414546b
zxh0 <zhangxiuhong2013@gmail.com>
2021-10-08 15:36:39 +0800

    Upgrade moeingevm& fix issue#25

--- go.mod
+++ go.mod
@@ -25,7 +25,7 @@ require (
 	github.com/rs/cors v1.7.0
 	github.com/smartbch/moeingads v0.3.0
 	github.com/smartbch/moeingdb v0.3.3-0.20210911235855-2eb460b7bb07
-	github.com/smartbch/moeingevm v0.3.1-0.20210912013724-6680354f6fd5
+	github.com/smartbch/moeingevm v0.3.1-0.20211008061422-326eba09b082
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.1.3
--- rpc/api/eth_api_utils.go
+++ rpc/api/eth_api_utils.go
@@ -125,9 +125,9 @@ func txToRpcResp(tx *types.Transaction) *rpctypes.Transaction {
 		Nonce:            hexutil.Uint64(tx.Nonce),
 		TransactionIndex: &idx,
 		Value:            (*hexutil.Big)(bigutils.U256FromSlice32(tx.Value[:]).ToBig()),
-		//V:
-		//R:
-		//S:
+		V:                (*hexutil.Big)(big.NewInt(int64(tx.V))),
+		R:                (*hexutil.Big)(bigutils.U256FromSlice32(tx.R[:]).ToBig()),
+		S:                (*hexutil.Big)(bigutils.U256FromSlice32(tx.S[:]).ToBig()),
 	}
 	copy(resp.BlockHash[:], tx.BlockHash[:])
 	if !isZeroAddress(tx.To) {
