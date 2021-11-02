7c2c5262ba5f6adcfe05068d52b11a262fb24239
ThiwakonPB <thiwakonmezenen@gmail.com>
2021-11-02 22:05:17 +0700

    Edit init-sync-node.sh to accept args to synce to genesis

--- init-sync-node.sh
+++ init-sync-node.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # check for args
-if [ "$#" -ne 1 ]
+if [ "$#" -ne 2 ]
 then
-  echo "Please specify introduction such as happyman"
+  echo "Please specify arguments in this order: Node_id , Genesis_IP"
   exit 1
 fi
 
 echo "Generating a private key"
 # docker-compose run smartbch gen-test-keys -n 10 > test-keys.txt
-docker-compose run smartbch gen-test-keys -n 1 --show-address | tee gen-test-keys3.txt
+docker-compose run smartbch_node gen-test-keys -n 1 --show-address | tee gen-test-keys3.txt
 echo
 
 echo "Generating consensus key info"
-docker-compose run smartbch generate-consensus-key-info | tee generate-consensus-key-info3.txt
+docker-compose run smartbch_node generate-consensus-key-info | tee generate-consensus-key-info3.txt
 # echo "Init the node, include the keys from the last step as a comma separated list."
 # docker-compose run smartbch init mynode --chain-id 0x2711 \
 #     --init-balance=10000000000000000000 \
@@ -28,18 +28,28 @@ IFS=' ' #space as delimiter
 read -ra BIT <<<"$GEN" #split string into :array name BIT
 EOF=${BIT[1]}
 K1=$(head -1 generate-consensus-key-info3.txt)
+echo
 # CPK=$(docker-compose run -w /root/.smartbchd/ smartbch generate-consensus-key-info)
 # docker-compose run --entrypoint mv smartbch /root/.smartbchd/priv_validator_key.json /root/.smartbchd/config
 # echo
-
-echo "Generate genesis validator info"
-
-docker-compose run smartbch generate-genesis-validator \
-    --validator-address=$EOF \
-    --consensus-pubkey=$K1 \
-    --voting-power=1 \
-    --staking-coin=0 \
-    --introduction=$1 | tee long-validator-info.txt
+echo "Sync node"
+docker-compose run smartbch_node init sync_node --chain-id 0x2711
 echo
-echo "Introduction: $1"
+
+echo "Configuring p2p seeds"
+seed=\"$1@$2:26656\"
+sudo sed -i "s/^seeds.*/seeds = $seed/" ./smartbch_node_data/config/config.toml
+echo
+
+echo "Configuring RPC"
+rpc=\"$2:8545\"
+sudo sed -i "s/^mainnet-rpc-url.*/mainnet-rpc-url = $rpc/" ./smartbch_node_data/config/app.toml
+echo
+
+echo "Moving genesis.json"
+sudo mv ./genesis.json ./smartbch_node_data/config
+echo
+
+
+
 echo "Finished!"
