1398329f1007fd6f9beba8b88803383ad0aef74a
nach <napat_pat3@hotmail.com>
2021-11-08 19:12:47 +0700

    feat: help checkpoint

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
 FROM ubuntu:20.04
 
+MAINTAINER Napat Charuphant <napat.charuphant1@gmail.com>
 
 RUN apt-get -y update && apt-get -y upgrade
+RUN apt-get -y install curl docker.io
+RUN curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+RUN chmod +x /usr/local/bin/docker-compose
+
+WORKDIR /usr/src/app
+COPY ./init-both-node.sh ./init-both-node.sh
+RUN chmod +x /usr/src/app/init-both-node.sh
 
 
-
+ENTRYPOINT ["/usr/src/app/init-both-node.sh"]
--- init-both-node.sh
+++ init-both-node.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# ==============
-# Genesis node
-# ==============
+echo "Generating wallet private keys"
+mkdir keys
+docker-compose -f /var/tmp/docker-compose.yml run smartbch_genesis gen-test-keys -n 10 > test-keys.txt
+echo
+
+echo cat
+cat test-keys.txt
+echo cat
+
+echo here is ls
+ls -a
+echo here is ls
 
 echo "=============="
 echo "Genesis Node"
+echo "Init the node, include the keys from the previous step as a comma separated list."
 echo "=============="
-echo "Generating Keys"
-mkdir keys 
-docker-compose run smartbch_genesis gen-test-keys -n 10 > test-keys.txt
-echo
-
- 
-
-echo "Init the node, include the keys from the last step as a comma separated list."
-
 # put json node id into json_node_id.txt file
 # then tail it out so that bash would recognize it as a string
 # there should be a better way to do this...
-NID=$(docker-compose run smartbch_genesis init mynode --chain-id 0x2711 \
+NID=$(docker-compose -f /var/tmp/docker-compose.yml run smartbch_genesis init mynode --chain-id 0x2711 \
     --init-balance=10000000000000000000 \
     --test-keys=`paste -d, -s test-keys.txt` \
     --home=/root/.smartbchd --overwrite \
@@ -27,35 +28,58 @@ NID=$(docker-compose run smartbch_genesis init mynode --chain-id 0x2711 \
     )
 echo
 
+echo tee
+tee json_node_id.txt
+echo tee
+
+
+echo here is NID
+echo $NID
+echo here is NID
+
+echo here is ls
+ls -a
+echo here is ls
+
 # getting nodeId from json_node_id file
 K1=$(tail -1 json_node_id.txt)
+echo here is cat
+cat json_node_id.txt
+echo here is cat
+
+echo here is k1
+echo $K1
+echo here is k1
 
 # splitting K1 for node Id
 IFS='\"' #colon as delimiter
 read -ra BIT <<<"$K1" # split string into :array name BIT
 NODEID=${BIT[11]} # choose index 11 of BIT array
+echo here is nodeid
+echo $NODEID
+echo here is nodeid
 
 echo Genesis node Id: $NODEID
 echo $NODEID > genesis_node_id.txt
 rm -fr json_node_id.txt
-CPK=$(docker-compose run -w /root/.smartbchd/ smartbch_genesis generate-consensus-key-info)
-docker-compose run --entrypoint mv smartbch_genesis /root/.smartbchd/priv_validator_key.json /root/.smartbchd/config
+CPK=$(docker-compose -f /var/tmp/docker-compose.yml run -w /root/.smartbchd/ smartbch_genesis generate-consensus-key-info)
+docker-compose -f /var/tmp/docker-compose.yml run --entrypoint mv smartbch_genesis /root/.smartbchd/priv_validator_key.json /root/.smartbchd/config
 echo
 
 echo "Generate genesis validator"
 
 K1=$(head -1 test-keys.txt)
-VAL=$(docker-compose run smartbch_genesis generate-genesis-validator $K1 \
+VAL=$(docker-compose -f /var/tmp/docker-compose.yml run smartbch_genesis generate-genesis-validator $K1 \
   --consensus-pubkey $CPK \
   --staking-coin 10000000000000000000000 \
   --voting-power 1 \
   --introduction "happygenesis" \
   --home /root/.smartbchd
   )
-docker-compose run smartbch_genesis add-genesis-validator --home=/root/.smartbchd $VAL
+docker-compose -f /var/tmp/docker-compose.yml run smartbch_genesis add-genesis-validator --home=/root/.smartbchd $VAL
 
 echo "Copy genesis.json"
-cp smartbch_genesis_data/config/genesis.json .
+cp /var/tmp/data/smartbch_genesis_data/config/genesis.json .
 mv ./test-keys.txt ./keys
 mv ./genesis_node_id.txt ./keys
 
@@ -72,23 +96,25 @@ echo "Sync Node"
 echo "=============="
 
 echo "Init chain id"
-docker-compose run smartbch_node init sync_node --chain-id 0x2711
+docker-compose -f /var/tmp/docker-compose.yml run smartbch_node init sync_node --chain-id 0x2711
+
 
 echo "Replace genesis.json"
-cp -fr genesis.json smartbch_node_data/config/.
+cp -fr genesis.json /var/tmp/data/smartbch_node_data/config/.
 rm genesis.json
 # get localhost ip
 
 # replacing line that starts with "seeds =" with $seed_address
 echo "Configuring p2p seeds"
 seed_address=\"$NODEID@smartbch_genesis:26656\"
-sed -i "s/^seeds =.*/seeds = $seed_address/" smartbch_node_data/config/config.toml
+echo $seed_address
+sed -i "s/^seeds =.*/seeds = $seed_address/" /var/tmp/data/smartbch_node_data/config/config.toml
 echo
 
 # replacing line that starts with "mainnet-rpc-url" with $rpc
 echo "Configuring RPC"
 rpc=\"smartbch_genesis:8545\"
-sudo sed -i "s/^mainnet-rpc-url.*/mainnet-rpc-url = $rpc/" ./smartbch_node_data/config/app.toml
+sed -i "s/^mainnet-rpc-url.*/mainnet-rpc-url = $rpc/" /var/tmp/data/smartbch_node_data/config/app.toml
 echo
 echo "Keys and genesis node id can be found at ./keys"
 echo "Sync node setup Finished!"
