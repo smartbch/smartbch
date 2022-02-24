c1f807ea66efd2d9db3efbe7f843138381b6110d
zxh0 <zhangxiuhong2013@gmail.com>
2022-02-24 10:09:21 +0800

    Simplify SEP109 truffle tests

--- testdata/sol/contracts/seps/TestSEP109.sol
+++ testdata/sol/contracts/seps/TestSEP109.sol
@@ -3,10 +3,10 @@ pragma solidity >=0.8.0;
 
 contract TestSEP109 {
 
-	function verify(uint alpha, bytes calldata pk, bytes calldata pi, bytes calldata beta) external returns (bool) {
+	function verify(uint alpha, bytes calldata pk, bytes calldata pi, uint beta) external view returns (bool) {
 		require(pk.length == 33, 'pk.length != 33');
-		(bool ok, bytes memory retData) = address(0x2713).call(abi.encodePacked(alpha, pk, pi));
-		return ok && keccak256(retData) == keccak256(beta);
+		(bool ok, bytes memory retData) = address(0x2713).staticcall(abi.encodePacked(alpha, pk, pi));
+		return ok && abi.decode(retData, (uint)) == beta;
 	}
 
 }
