d6c2a7ec338a08e4ee672b5d1ff0af901d6cb432
zxh0 <zhangxiuhong2013@gmail.com>
2022-04-05 22:22:17 +0800

    Update XHedgeContractSequence

--- param/params.go
+++ param/params.go
@@ -40,7 +40,7 @@ const (
 	AmberBlocksInEpochAfterXHedgeFork int64 = 2016 * 10 * 60 / 6
 
 	// fork params
-	XHedgeContractSequence uint64 = 0x10651
+	XHedgeContractSequence uint64 = 0x109af
 	XHedgeForkBlock        int64  = 3812000
 	ShaGateForkBlock       int64  = 80000000
 	ShaGateSwitch          bool   = false
