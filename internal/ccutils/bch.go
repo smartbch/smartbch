ffd6b1a2431737b9098be46ee894053b4746a54a
zxh0 <zhangxiuhong2013@gmail.com>
2022-06-28 10:43:00 +0800

    Add MakeMultiSigUnsignedRedeemTx()

--- internal/ccutils/bch.go
+++ internal/ccutils/bch.go
@@ -1,11 +1,15 @@
 package ccutils
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 
+	"github.com/gcash/bchd/bchec"
 	"github.com/gcash/bchd/chaincfg"
+	"github.com/gcash/bchd/chaincfg/chainhash"
 	"github.com/gcash/bchd/txscript"
+	"github.com/gcash/bchd/wire"
 	"github.com/gcash/bchutil"
 )
 
@@ -69,6 +73,68 @@ func GetMultiSigRedeemScript(redeemScriptWithoutConstructorArgs string,
 	return builder.Script()
 }
 
-func MakeMultiSigUnsignedRedeemTx() {
+func MakeMultiSigUnsignedRedeemTx(redeemScript []byte,
+	txid string, vout uint32, toAddr string, outValue int64) (*wire.MsgTx, error) {
 
+	//prevOutValue := int64(10000)
+	//redeemOutValue := int64(6000)
+
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+
+	// you should provide your UTXO hash
+	utxoHash, err := chainhash.NewHashFromStr(txid)
+	if err != nil {
+		return nil, err
+	}
+
+	// and add the index of the UTXO
+	outPoint := wire.NewOutPoint(utxoHash, vout)
+	txIn := wire.NewTxIn(outPoint, nil)
+	redeemTx.AddTxIn(txIn)
+
+	// adding the output to tx
+	decodedAddr, err := bchutil.DecodeAddress(toAddr, &chaincfg.MainNetParams)
+	if err != nil {
+		return nil, err
+	}
+	destinationAddrByte, err := txscript.PayToAddrScript(decodedAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	// adding the destination address and the amount to the transaction
+	redeemTxOut := wire.NewTxOut(outValue, destinationAddrByte)
+	redeemTx.AddTxOut(redeemTxOut)
+
+	//buf := bytes.NewBuffer(make([]byte, 0, redeemTx.SerializeSize()))
+	//_ = redeemTx.Serialize(buf)
+	return redeemTx, nil
+}
+
+func SignRedeemTx(redeemTx *wire.MsgTx, redeemScript []byte, prevOutValue int64, key *bchec.PrivateKey) ([]byte, error) {
+	return txscript.RawTxInECDSASignature(redeemTx, 0, redeemScript, txscript.SigHashAll|txscript.SigHashForkID, key, prevOutValue)
+}
+
+func FixMultiSigUnsignedRedeemTx(redeemTx *wire.MsgTx, redeemScript []byte, sigs [][]byte) (string, error) {
+	signature := txscript.NewScriptBuilder()
+	//signature.AddOp(txscript.OP_FALSE)
+	for _, sig := range sigs {
+		signature.AddData(sig)
+	}
+	signature.AddInt64(1) // selector
+	signature.AddData(redeemScript)
+	signatureScript, err := signature.Script()
+	if err != nil {
+		// Handle the error.
+		return "", err
+	}
+
+	redeemTx.TxIn[0].SignatureScript = signatureScript
+
+	var signedTx bytes.Buffer
+	_ = redeemTx.Serialize(&signedTx)
+
+	hexSignedTx := hex.EncodeToString(signedTx.Bytes())
+
+	return hexSignedTx, nil
 }
