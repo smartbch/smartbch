7e6c706d579bc8dd70938555783cd088f701f46c
zxh0 <zhangxiuhong2013@gmail.com>
2022-06-30 10:13:51 +0800

    fix error info of blocked address

--- app/app.go
+++ app/app.go
@@ -330,7 +330,7 @@ func (app *App) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx 
 		app.sigCacheAdd(txid, SenderAndHeight{sender, app.currHeight})
 	}
 	if sender == ebp.BlockedAddress {
-		return abcitypes.ResponseCheckTx{Code: CannotRecoverSender, Info: "invalid sender: " + err.Error()}
+		return abcitypes.ResponseCheckTx{Code: CannotRecoverSender, Info: "invalid sender: " + sender.String()}
 	}
 	return app.checkTxWithContext(tx, sender, req.Type)
 }
