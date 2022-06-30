362416d7131267224bca5b99ab0870761438914b
Kui Wang <wangkui0508@hotmail.com>
2022-06-30 09:00:04 +0800

    go fmt

--- app/app.go
+++ app/app.go
@@ -329,6 +329,9 @@ func (app *App) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx 
 		}
 		app.sigCacheAdd(txid, SenderAndHeight{sender, app.currHeight})
 	}
+	if sender == ebp.BlockedAddress {
+		return abcitypes.ResponseCheckTx{Code: CannotRecoverSender, Info: "invalid sender: " + err.Error()}
+	}
 	return app.checkTxWithContext(tx, sender, req.Type)
 }
 
--- go.mod
+++ go.mod
@@ -25,7 +25,7 @@ require (
 	github.com/rs/cors v1.7.0
 	github.com/smartbch/moeingads v0.4.2
 	github.com/smartbch/moeingdb v0.4.2
-	github.com/smartbch/moeingevm v0.4.2
+	github.com/smartbch/moeingevm v0.4.3-0.20220630003522-cd7f07265daa
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.1.3
