1a0fa67c8b9ccce20917f4b26caa05e031c51382
zxh0 <zhangxiuhong2013@gmail.com>
2022-11-24 14:34:08 +0800

    Add SignRedeemTxSigHashECDSA

--- crosschain/covenant/tx_utils.go
+++ crosschain/covenant/tx_utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 
+	"github.com/gcash/bchd/bchec"
 	"github.com/gcash/bchd/txscript"
 	"github.com/gcash/bchd/wire"
 	"github.com/gcash/bchutil"
@@ -25,8 +26,18 @@ func SignCcCovenantTxSigHashECDSA(wifStr string, hash []byte, hashType txscript.
 	if err != nil {
 		return nil, err
 	}
+	return signCcCovenantTxSigHashECDSA(wif.PrivKey, hash, hashType)
+}
 
-	signature, err := wif.PrivKey.SignECDSA(hash)
+func SignRedeemTxSigHashECDSA(privKey *bchec.PrivateKey, hash []byte) ([]byte, error) {
+	hashType := txscript.SigHashAll | txscript.SigHashForkID
+	return signCcCovenantTxSigHashECDSA(privKey, hash, hashType)
+}
+
+func signCcCovenantTxSigHashECDSA(privKey *bchec.PrivateKey,
+	hash []byte, hashType txscript.SigHashType) ([]byte, error) {
+
+	signature, err := privKey.SignECDSA(hash)
 	if err != nil {
 		return nil, fmt.Errorf("cannot sign tx input: %s", err)
 	}
