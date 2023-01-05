6d774abefd013fc61c66aec49ddfb38349e08391
helldealer <lhrmachinery@163.com>
2023-01-05 16:10:06 +0800

    Restart test

--- crosschain/types/types.go
+++ crosschain/types/types.go
@@ -26,40 +26,40 @@ const (
 )
 
 type UTXORecord struct {
-	OwnerOfLost      [20]byte `msgp:"owner_of_lost"`
-	CovenantAddr     [20]byte `msgp:"covenant_addr"`
-	IsRedeemed       bool     `msgp:"is_redeemed"`
-	RedeemTarget     [20]byte `msgp:"redeem_target"`
-	ExpectedSignTime int64    `msgp:"expected_sign_time"`
-	Txid             [32]byte `msgp:"txid"`
-	Index            uint32   `msgp:"index"`
-	Amount           [32]byte `msgp:"amount"`
-	BornTime         int64    `msgp:"born_time"`
+	OwnerOfLost      [20]byte
+	CovenantAddr     [20]byte
+	IsRedeemed       bool
+	RedeemTarget     [20]byte
+	ExpectedSignTime int64
+	Txid             [32]byte
+	Index            uint32
+	Amount           [32]byte
+	BornTime         int64
 }
 
 type CCContext struct {
-	MonitorsWithPauseCommand   [][20]byte `msgp:"monitor_with_pause_command"`
-	RescanTime                 int64      `msgp:"rescan_time"`                    // last startRescan block timestamp, init is max int64
-	RescanHeight               uint64     `msgp:"rescan_height"`                  // main chain block height used as rescan end height, init is shaGate enabling height
-	LastRescannedHeight        uint64     `msgp:"last_rescanned_height"`          // main chain block height used as rescan start height, init is 0
-	UTXOAlreadyHandled         bool       `msgp:"utxo_already_handled"`           // set when call handleUtxo, unset when call startRescan, init is true
-	TotalBurntOnMainChain      [32]byte   `msgp:"total_burnt_on_main_chain"`      // init is totalBurnt BCH when shaGate enabling
-	TotalMinerFeeForConvertTx  [32]byte   `msgp:"total_miner_fee_for_convert_tx"` // init is zero, the accumulative miner fee used to utxo convert tx, which is covered by side chain black hole balance
-	LastCovenantAddr           [20]byte   `msgp:"last_covenant_addr"`             // init is zero address
-	CurrCovenantAddr           [20]byte   `msgp:"curr_covenant_addr"`             // init is genesis covenant address
-	LatestEpochHandled         int64      `msgp:"latest_epoch_handled"`           // init is zero, the latest epoch number handled for operator or monitor election
-	CovenantAddrLastChangeTime int64      `msgp:"covenant_addr_last_change_time"` // init is zero, the latest covenant addr change side chain block timestamp
+	MonitorsWithPauseCommand   [][20]byte
+	RescanTime                 int64    // last startRescan block timestamp, init is max int64
+	RescanHeight               uint64   // main chain block height used as rescan end height, init is shaGate enabling height
+	LastRescannedHeight        uint64   // main chain block height used as rescan start height, init is 0
+	UTXOAlreadyHandled         bool     // set when call handleUtxo, unset when call startRescan, init is true
+	TotalBurntOnMainChain      [32]byte // init is totalBurnt BCH when shaGate enabling
+	TotalMinerFeeForConvertTx  [32]byte // init is zero, the accumulative miner fee used to utxo convert tx, which is covered by side chain black hole balance
+	LastCovenantAddr           [20]byte // init is zero address
+	CurrCovenantAddr           [20]byte // init is genesis covenant address
+	LatestEpochHandled         int64    // init is zero, the latest epoch number handled for operator or monitor election
+	CovenantAddrLastChangeTime int64    // init is zero, the latest covenant addr change side chain block timestamp
 }
 
 type CCInternalInfosForTest struct {
-	TotalRedeemAmountS2M       [32]byte `msgp:"total_redeem_amount_s2m"`
-	TotalRedeemNumsS2M         uint64   `msgp:"total_redeem_nums_s2m"`
-	TotalLostAndFoundAmountS2M [32]byte `msgp:"total_lost_and_found_amount_s2m"`
-	TotalLostAndFoundNumsS2M   uint64   `msgp:"total_lost_and_found_nums_s2m"`
-	TotalTransferAmountM2S     [32]byte `msgp:"total_transfer_amount_m2s"`
-	TotalTransferNumsM2S       uint64   `msgp:"total_transfer_nums_m2s"`
-	TotalTransferByBurnAmount  [32]byte `msgp:"total_transfer_by_burn_amount"`
-	TotalTransferByBurnNums    uint64   `msgp:"total_transfer_by_burn_nums"`
+	TotalRedeemAmountS2M       [32]byte
+	TotalRedeemNumsS2M         uint64
+	TotalLostAndFoundAmountS2M [32]byte
+	TotalLostAndFoundNumsS2M   uint64
+	TotalTransferAmountM2S     [32]byte
+	TotalTransferNumsM2S       uint64
+	TotalTransferByBurnAmount  [32]byte
+	TotalTransferByBurnNums    uint64
 }
 
 type CCInfosForTest struct {
@@ -96,15 +96,15 @@ const (
 )
 
 type MonitorVoteInfo struct {
-	Number      int64         `msgp:"number"` // same with epoch number, start from param.StartEpochNumberForCC
-	StartHeight int64         `msgp:"start_height"`
-	EndTime     int64         `msgp:"end_time"`
-	Nominations []*Nomination `msgp:"nominations"`
+	Number      int64 // same with epoch number, start from param.StartEpochNumberForCC
+	StartHeight int64
+	EndTime     int64
+	Nominations []*Nomination
 }
 
 type Nomination struct {
-	Pubkey         [33]byte `msgp:"pubkey"` // The monitor's compressed pubkey used in main chain
-	NominatedCount int64    `msgp:"nominated_count"`
+	Pubkey         [33]byte // The monitor's compressed pubkey used in main chain
+	NominatedCount int64
 }
 
 type UTXOCollectParam struct {
